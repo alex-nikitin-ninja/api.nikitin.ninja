7653aecb51fa477332998f6a75049f7f432df892
Alex <alex.nikitin.2015@gmail.com>
2018-01-07 18:12:03 -0800

    minor updates

--- app/helpers/scrapeCarsHelper.php
+++ app/helpers/scrapeCarsHelper.php
@@ -1,6 +1,30 @@
 <?php
 Class scrapeCarsHelper extends Helper{
 
+	// CREATE TABLE nikitin_ninja.tbl_scraped_cars (
+	//     id BIGINT AUTO_INCREMENT,
+	//     ad_caption VARCHAR(256),
+	//     ad_direct_url VARCHAR(512),
+	//     ad_time_mysql DATETIME,
+	//     ad_time_raw VARCHAR(64),
+	//     ad_time_parsed DATETIME,
+	//     car_price_parsed DECIMAL(10, 2),
+	//     car_price_raw VARCHAR(63),
+	//     car_year VARCHAR(16),
+	//     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	//     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	//     deleted_at DATETIME,
+	//     PRIMARY KEY (id)
+	// );
+
+	// CREATE TABLE nikitin_ninja.tbl_scraped_cars_images (
+	//     id BIGINT AUTO_INCREMENT,
+	//     car_id BIGINT,
+	//     big VARCHAR(512),
+	//     sm VARCHAR(512),
+	//     PRIMARY KEY (id)
+	// );
+
 	public function storeData($getParams, $postParams){
 		// $alertsModel = new messagesModel();
 		$r = array(
