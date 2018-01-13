2c9df4904e58429c47cdcf052bf1945cc67152d3
Alex <alex.nikitin.2015@gmail.com>
2018-01-13 10:15:54 -0800

    moree data

--- app/helpers/scrapeJobsHelper.php
+++ app/helpers/scrapeJobsHelper.php
@@ -1,43 +1,42 @@
 <?php
 Class scrapeJobsHelper extends Helper{
 	// /home/ubuntu/shell/docker/run-scheduled-tasks.sh
-	// curl -s -H "Content-Type: application/json" -X POST -d @results-scraping.json https://api.nikitin.ninja/v1/scrapeCars/storeData
+	// curl -s -H "Content-Type: application/json" -X POST -d @results-scraping.json https://api.nikitin.ninja/v1/scrapeJobs/storeData
 	public function storeData($getParams, $postParams){
 		$scrapeJobsModel = new scrapeJobsModel();
 		$listOfAds = $postParams;
 
-		$newCars = 0;
-		foreach ($listOfAds as $oneCar) {
-			$carInfo = $scrapeJobsModel->getJobByUrl($oneCar['adDirectUrl'], ['ad_direct_url']);
-			// if(count($carInfo) === 0) {
-			// 	$row = [
-			// 		"ad_caption"       => $oneCar['adCaption'],
-			// 		"ad_direct_url"    => $oneCar['adDirectUrl'],
-			// 		"ad_time_mysql"    => $oneCar['adTime']['mysql'],
-			// 		"ad_time_raw"      => $oneCar['adTime']['raw'],
-			// 		"ad_time_parsed"   => $oneCar['adTime']['mysql'],
-			// 		"car_price_parsed" => $oneCar['carPrice']['parsed'],
-			// 		"car_price_raw"    => $oneCar['carPrice']['raw'],
-			// 		"car_year"         => $oneCar['carYear'],
-			// 		"raw_data"         => json_encode($oneCar),
-			// 	];
-			// 	// $carId = $scrapeJobsModel->makeInsert('nikitin_ninja.tbl_scraped_cars', $row);
+		$newAds = 0;
+		foreach ($listOfAds as $oneAd) {
+			$adInfo = $scrapeJobsModel->getJobByUrl($oneAd['adDirectUrl'], ['ad_direct_url']);
+			if(count($adInfo) === 0) {
+				$row = [
+					"ad_caption"     => $oneAd['adCaption'],
+					"ad_location"    => $oneAd['adLocation'],
+					"ad_map_id"      => $oneAd['adMapId'],
+					"ad_direct_url"  => $oneAd['adDirectUrl'],
+					"ad_time_mysql"  => $oneAd['adTime']['mysql'],
+					"ad_time_raw"    => $oneAd['adTime']['raw'],
+					"ad_time_parsed" => $oneAd['adTime']['mysql'],
+					"raw_data"       => json_encode($oneAd),
+				];
+				$adId = $scrapeJobsModel->makeInsert('nikitin_ninja.tbl_scraped_jobs', $row);
 
-			// 	// foreach ($oneCar['carImages'] as $oneImage) {
-			// 	// 	$imgRow = [
-			// 	// 		"car_id" => $carId,
-			// 	// 		"big"    => $oneImage['big'],
-			// 	// 		"sm"     => $oneImage['sm'],
-			// 	// 	];
-			// 	// 	$scrapeJobsModel->makeInsert('nikitin_ninja.tbl_scraped_cars_images', $imgRow);
-			// 	// }
-			// 	$newCars++;
-			// }
+				foreach ($oneAd['adImages'] as $oneImage) {
+					$imgRow = [
+						"job_id" => $adId,
+						"big"    => $oneImage['big'],
+						"sm"     => $oneImage['sm'],
+					];
+					$scrapeJobsModel->makeInsert('nikitin_ninja.tbl_scraped_jobs_images', $imgRow);
+				}
+				$newAds++;
+			}
 		}
 
 		$r = array(
-			"recvCars" => count($listOfAds),
-			"newCars"  => $newCars,
+			"recvAds" => count($listOfAds),
+			"newAds"  => $newAds,
 		);
 		return $r;
 	}
--- app/models/scrapeJobsModel.php
+++ app/models/scrapeJobsModel.php
@@ -1,24 +1,24 @@
 <?php
 Class scrapeJobsModel extends Model {
-	// CREATE TABLE nikitin_ninja.tbl_scraped_cars (
+	// CREATE TABLE nikitin_ninja.tbl_scraped_jobs (
 	//     id BIGINT AUTO_INCREMENT,
 	//     ad_caption VARCHAR(256),
+	//     ad_location VARCHAR(256),
+	//     ad_map_id VARCHAR(256),
 	//     ad_direct_url VARCHAR(512),
 	//     ad_time_mysql DATETIME,
 	//     ad_time_raw VARCHAR(64),
 	//     ad_time_parsed DATETIME,
-	//     car_price_parsed DECIMAL(10, 2),
-	//     car_price_raw VARCHAR(63),
-	//     car_year VARCHAR(16),
+	//     raw_data MEDIUMTEXT,
 	//     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 	//     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	//     deleted_at DATETIME,
 	//     PRIMARY KEY (id)
 	// );
 
-	// CREATE TABLE nikitin_ninja.tbl_scraped_cars_images (
+	// CREATE TABLE nikitin_ninja.tbl_scraped_jobs_images (
 	//     id BIGINT AUTO_INCREMENT,
-	//     car_id BIGINT,
+	//     job_id BIGINT,
 	//     big VARCHAR(512),
 	//     sm VARCHAR(512),
 	//     PRIMARY KEY (id)
@@ -39,7 +39,7 @@ Class scrapeJobsModel extends Model {
 			"SELECT
 				{$columns}
 			FROM
-				nikitin_ninja.tbl_scraped_cars t
+				nikitin_ninja.tbl_scraped_jobs t
 			WHERE
 				ad_direct_url LIKE ':adDirectUrl'";
 
